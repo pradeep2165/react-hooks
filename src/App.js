cda035c66a0d015f0e134cfaf096676dab3b7e19
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-17 09:50:46 +0530

    unsafe update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import ClassCounter from "./component/ClassCounter";
 import HooksCounter from "./component/HooksCounter";
+import HookCounterTwo from "./component/HookCounterTwo";
 
 function App() {
   return (
     <div className="App">
-      <ClassCounter />
-      <HooksCounter />
+      {/* <ClassCounter />
+      <HooksCounter /> */}
+      <HookCounterTwo />
     </div>
   );
 }
---
+++ src/component/HookCounterTwo.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+function HookCounterTwo() {
+  const initialValue = 0;
+  const [value, setValue] = useState(initialValue);
+  const incrementFive = () => {
+    for (let i = 0; i < 5; i++) {
+      setValue(value + 1);
+    }
+  };
+  return (
+    <div>
+      <div>{value}</div>
+      <button onClick={() => setValue(value + 1)}>increment</button>
+      <button onClick={() => setValue(value - 1)}>decrement</button>
+      <button onClick={incrementFive}>increment 5</button>
+      <button onClick={() => setValue(initialValue)}>reset</button>
+    </div>
+  );
+}
+
+export default HookCounterTwo;
