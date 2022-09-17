51e53a06a6edbec83dcd34a7cb21323ec6f2e037
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-17 10:28:45 +0530

    now updating perfectly

--- src/component/HookCounterThree.js
+++ src/component/HookCounterThree.js
@@ -5,8 +5,8 @@ function HookCounterThree() {
 
   return (
     <form>
-      <input type="text" value={name.firstName} onChange={(e) => setName({ firstName: e.target.value })} />
-      <input type="text" value={name.lastName} onChange={(e) => setName({ lastName: e.target.value })} />
+      <input type="text" value={name.firstName} onChange={(e) => setName({ ...name, firstName: e.target.value })} />
+      <input type="text" value={name.lastName} onChange={(e) => setName({ ...name, lastName: e.target.value })} />
       <h2>Your first name is - {name.firstName}</h2>
       <h2>Your last name is - {name.lastName}</h2>
       <h1>{JSON.stringify(name)}</h1>
