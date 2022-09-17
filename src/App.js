61435db7968d300209cfb9b0365f03cfbc3ede17
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-17 10:25:32 +0530

    useState used in object value

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import ClassCounter from "./component/ClassCounter";
 import HooksCounter from "./component/HooksCounter";
 import HookCounterTwo from "./component/HookCounterTwo";
 import ClassCounterTwo from "./component/ClassCounterTwo";
+import HookCounterThree from "./component/HookCounterThree";
 
 function App() {
   return (
     <div className="App">
       {/* <ClassCounter />
       <HooksCounter /> */}
-      <ClassCounterTwo />
-      <HookCounterTwo />
+      {/* <ClassCounterTwo /> */}
+      {/* <HookCounterTwo /> */}
+      <HookCounterThree />
     </div>
   );
 }
---
+++ src/component/HookCounterThree.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+function HookCounterThree() {
+  const [name, setName] = useState({ firstName: "", lastName: "" });
+
+  return (
+    <form>
+      <input type="text" value={name.firstName} onChange={(e) => setName({ firstName: e.target.value })} />
+      <input type="text" value={name.lastName} onChange={(e) => setName({ lastName: e.target.value })} />
+      <h2>Your first name is - {name.firstName}</h2>
+      <h2>Your last name is - {name.lastName}</h2>
+      <h1>{JSON.stringify(name)}</h1>
+    </form>
+  );
+}
+
+export default HookCounterThree;
