12e838861358a95e30919511fecb62a722e05274
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-17 09:55:52 +0530

    safe update using prevValue with passing a arrow function

--- src/component/HookCounterTwo.js
+++ src/component/HookCounterTwo.js
@@ -5,14 +5,14 @@ function HookCounterTwo() {
   const [value, setValue] = useState(initialValue);
   const incrementFive = () => {
     for (let i = 0; i < 5; i++) {
-      setValue(value + 1);
+      setValue((prevValue) => prevValue + 1);
     }
   };
   return (
     <div>
       <div>{value}</div>
-      <button onClick={() => setValue(value + 1)}>increment</button>
-      <button onClick={() => setValue(value - 1)}>decrement</button>
+      <button onClick={() => setValue((prevValue) => prevValue + 1)}>increment</button>
+      <button onClick={() => setValue((prevValue) => prevValue - 1)}>decrement</button>
       <button onClick={incrementFive}>increment 5</button>
       <button onClick={() => setValue(initialValue)}>reset</button>
     </div>
