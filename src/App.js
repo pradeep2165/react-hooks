b91a117a485b8009d0fd0e59af97a4c1686581be
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-17 12:51:57 +0530

    run useEffect only once

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import ClassCounterOne from "./component/useEffect/class/ClassCounterOne";
 import HookCounter from "./component/useEffect/functional/HookCounter";
+import HookMouse from "./component/useEffect/HookMouse";
 
 function App() {
   return (
     <div className="App">
-      <ClassCounterOne />
-      <HookCounter />
+      {/* <ClassCounterOne />
+      <HookCounter /> */}
+      <HookMouse />
     </div>
   );
 }
