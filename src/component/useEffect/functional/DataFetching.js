dc1a37f28a6b61316b7cc49bfd499ac1b0660271
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-17 16:02:29 +0530

    single data fetching

--- src/component/useEffect/functional/DataFetching.js
+++ src/component/useEffect/functional/DataFetching.js
@@ -1,26 +1,28 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 function DataFetching() {
-  const [posts, setPosts] = useState([]);
-
+  const [posts, setPosts] = useState({});
+  const [id, setId] = useState([1]);
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts")
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then((res) => {
         setPosts(res.data);
       })
       .catch((error) => {
         console.log(error.message);
       });
-  }, []);
+  }, [id]);
   console.log(posts);
   return (
     <div>
-      <ul>
+      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      {/* <ul>
         {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
         ))}
-      </ul>
+      </ul> */}
+      <p>{posts.title}</p>
     </div>
   );
 }
