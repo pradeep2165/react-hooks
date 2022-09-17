574065b387a7d77a715b3c286d5a0c1d8b34c080
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-17 15:30:27 +0530

    useEffect with incorrect dependency

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import ClassCounterOne from "./component/useEffect/class/ClassCounterOne";
 import HookCounter from "./component/useEffect/functional/HookCounter";
 import HookMouse from "./component/useEffect/functional/HookMouse";
 import MouseContainer from "./component/useEffect/functional/MouseContainer";
+import IntervalClassCounter from "./component/useEffect/class/IntervalClassCounter";
+import IntervalHookCounter from "./component/useEffect/functional/IntervalHookCounter";
 
 function App() {
   return (
     <div className="App">
       {/* <ClassCounterOne />
       <HookCounter /> */}
-      <MouseContainer />
+      {/* <MouseContainer /> */}
+      <IntervalClassCounter />
+      <IntervalHookCounter />
     </div>
   );
 }
---
+++ src/component/useEffect/functional/IntervalHookCounter.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+
+function IntervalHookCounter() {
+  const [count, setCount] = useState(0);
+
+  const tick = () => [setCount((count) => count + 1)];
+  useEffect(() => {
+    const interval = setInterval(tick, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  return <div>{count}</div>;
+}
+
+export default IntervalHookCounter;
